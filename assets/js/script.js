4282cb8f93b361e0eb88ffff37b4c110ae6a734a
colmhaugh <colm.haugh@gmail.com>
2021-08-20 13:24:43 +0000

    add function to add or remove class when clicked using classList.toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
+/*  make card flipCard when clicked
+ create a list of the memory cards and store it in cards.
+ for each of the cards in the list we call the flipCard function.
+ adding an event listner to listen out for a click on the card
+ When  a card is clicked, it will call flipCard function */
+
 const cards = document.querySelectorAll(".memory-card");
 
 function flipCard(){
-    console.log("Test Click");
+    this.classList.toggle('flip');
 }
 
-cards.forEach(flipCard)
-{
-    addEventListener('click', flipCard);
-}
+cards.forEach(card => card.addEventListener('click', flipCard));
