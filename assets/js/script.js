fcc39b56a75e5894a391592c258c996289f2208b
colmhaugh <colm.haugh@gmail.com>
2021-08-26 19:09:48 +0000

    add modual instead of prompt for showing user instrucions

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ let hasFlippedCard = false;
 let firstCard, secondCard;
 
 /* Pop up message */
-function userAlert() { 
+/* function userAlert() { 
     userName = prompt('Please enter your name', String);  
     alert(`
     Hi ${userName}, 
@@ -27,7 +27,7 @@ function userAlert() {
     Please turn over one card and then try to find a matching card.     `
     ); 
    
-}
+} */
 
 
 
@@ -133,3 +133,26 @@ function reset(){
     
 }
 
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+function userAlert() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
