b1100fe79d76d144b01cd5c19e7fb99073b9358a
colmhaugh <colm.haugh@gmail.com>
2021-08-26 23:53:41 +0000

    remove unused commented out code

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,42 +14,43 @@ let firstCard, secondCard;
 var modal = document.getElementById("myModal");
 var endModal = document.getElementById("endModal");
 
-/*Using toggle to add flip class to memory card that was selected and removing it if it is present*/
+//Using classList.add to add flip class to memory card that was selected and removing it if it is present
 
 function flipCard(){
     
-    /* add flip to card thats selected; */
+    // add flip to card thats selected;
 
     this.classList.add('flip');
-        /* click a card, if it is the first card turned it will become the first card flipped */
+        // click a card, if it is the first card turned it will become the first card flipped
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
         return;
     }
-        /* when the first card is flipped and the player selects the another card, it will become secondCard */        
+        /* when the first card is flipped and the player selects the another card, 
+        it will become secondCard */        
     secondCard = this;
 
-        /* has flipped will then go back to false */
+        // has flipped will then go back to false
     hasFlippedCard = false;
 
-    /* player has now selected two cards.  Call a function to check if they are matched */
+    // player has now selected two cards.  Call a function to check if they are matched
     checkForMatch();
 }
 
 function checkForMatch(){
-    /* Check if the dataset-band from the memory card on firstCard and secondCard match */
-    if (firstCard.dataset.band === secondCard.dataset.band){        
-        /*if the cards match then leave the cards faced up and remove the click option */
+    // Check if the dataset-band from the memory card on firstCard and secondCard match
+    if (firstCard.dataset.band === secondCard.dataset.band){      
+
+    // if the cards match then call the function that will leave the cards faced up and remove the click option
         disableCards();
 
-        /* Play clip from album */
+        // Play clip from album
         playSound();
         noOfCards = noOfCards - 2;     
         if (noOfCards === 0 && noIncorrectCards <= 3 ){
             document.getElementById("endGameModalParagraph").innerHTML = "CONGRATLATIONS!  You have a great memory";
-            endGame();}
-            
+            endGame();}            
 
             else if (noOfCards == 0 && noIncorrectCards <= 7 ){
             document.getElementById("endGameModalParagraph").innerHTML = "Well Done, You have a fine memory";
@@ -61,18 +62,20 @@ function checkForMatch(){
                   
         return;
      }   
-    /*if the cards don't match then flip the cards back over again */
+    // if the cards don't match then call the function that will flip the cards back over again
     unflipCards();
     }
 
-/* Function called when the cards matched.  It removed the event listener so the cards can't be selected again */
+/* Function called when the cards matched.  
+It removed the event listener so the cards can't be selected again */
 function disableCards(){
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
     
 }
 
-/* Function called when the cards dont match.  We remove the flip class that was added when they were selected.  Cards flip back after half a second */
+/* Function called when the cards dont match.  We remove the flip class that was added when they were selected.  
+Cards flip back after half a second */
 function unflipCards(){
     setTimeout(() => {
         firstCard.classList.remove('flip');
@@ -83,11 +86,11 @@ function unflipCards(){
 
 }
 
-    /* listens out for user to click a card and then calls flipCard function */
+// istens out for user to click a card and then calls flipCard function
 cards.forEach(card => card.addEventListener('click', flipCard));
 
 
-    /* shuffle cards to create a rand list of cards, Start button calls this function and the game begins */
+// shuffle cards to create a rand list of cards, Start button calls this function and the game begins */
 function shuffleCards() {    
     noOfCards = 12;
     noIncorrectCards = 0;
@@ -97,7 +100,7 @@ function shuffleCards() {
     });
 
 }
- /* Function to play sound when cards are matched */
+ // Function to play sound when cards are matched
 function playSound(){
     var x = document.getElementById("myAudio");   
     x.play(); 
